78d904ef8964e91ee876d431a6180c9db86d4e9e
chemars <37469933+chemars@users.noreply.github.com>
2018-07-15 21:00:52 +0800

    增加下載方式

--- record_download.bat
+++ record_download.bat
@@ -3,4 +3,5 @@ SET "url=https://raw.githubusercontent.com/chemars/TV_EEW_Record/master/record.a
 SET "filename=record_download.ahk"
 
 PowerShell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%~dp0%filename%');"
+IF NOT EXIST "%~dp0%filename%" PowerShell -WindowStyle Hidden -Command "Invoke-WebRequest -OutFile '%~dp0%filename%' '%url%'"
 START "" "%~dp0%filename%"
