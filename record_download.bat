4db8b23a5b0acb93512dbfc008eb27c9e37fe5f9
chemars <37469933+chemars@users.noreply.github.com>
2018-07-16 09:54:27 +0800

    錯誤修正

--- record_download.bat
+++ record_download.bat
@@ -1,11 +1,15 @@
 @ECHO OFF
 SET "url=https://raw.githubusercontent.com/chemars/TV_EEW_Record/master/record.ahk"
 SET "filename=record_download.ahk"
+SET "old_file=old_file.ahk"
 
+IF EXIST "%~dp0%filename%" COPY "%~dp0%filename%" "%~dp0%old_file%"
 PowerShell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%~dp0%filename%');"
 IF NOT EXIST "%~dp0%filename%" PowerShell -WindowStyle Hidden -Command "Invoke-WebRequest -OutFile '%~dp0%filename%' '%url%'"
 IF EXIST "%~dp0%filename%" (
     START "" "%~dp0%filename%"
+    IF EXIST "%~dp0%old_file%" DEL "%~dp0%old_file%"
  ) ELSE ( 
-    Echo 下載失敗 >> "%~dp0error.txt"
+    Echo 下載失敗，嘗試使用舊檔案 >> "%~dp0error.txt"
+    IF EXIST "%~dp0%old_file%" START "" "%~dp0%old_file%"
  )
