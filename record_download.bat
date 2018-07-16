42edf0cb638a59bb4c84be89a6c044d58f69a6ca
chemars <37469933+chemars@users.noreply.github.com>
2018-07-16 09:09:34 +0800

    修正錯誤

--- record_download.bat
+++ record_download.bat
@@ -4,4 +4,8 @@ SET "filename=record_download.ahk"
 
 PowerShell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%~dp0%filename%');"
 IF NOT EXIST "%~dp0%filename%" PowerShell -WindowStyle Hidden -Command "Invoke-WebRequest -OutFile '%~dp0%filename%' '%url%'"
-START "" "%~dp0%filename%"
+IF EXIST "%~dp0%filename%" (
+    START "" "%~dp0%filename%"
+ ) ELSE ( 
+    Echo ¤U¸ü¥¢±Ñ >> "%~dp0error.txt"
+ )
